668638e146a00ef4eac630c2215ce0c539271b72
Recxo <rishava065@gmail.com>
2023-09-09 20:55:42 +0530

    commit

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class BlogPost(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
     participants = models.ManyToManyField(
         User, related_name='participants', blank=True)
     # slug = models.CharField(max_length=200,null=False)
